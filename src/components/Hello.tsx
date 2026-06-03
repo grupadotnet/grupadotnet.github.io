35b286681b58df87881d7f2771cc78bf1550624b
MooZeK <mooz.05@hotmail.com>
2026-06-03 20:05:36 +0200

    Restructurised pages

--- src/components/Hello.tsx
+++
@@ -1,9 +0,0 @@
-export default function Hello() {
-    return(
-        <>
-            <div className={"from-(--primary-from) to-(--primary-to) bg-linear-[201.03deg] h-[100vh] w-full flex"} id={"HELLO"}>
-                <p className={"text-[clamp(1rem,7vw,8rem)] mt-[35vh] w-7/8 m-auto text-balance text-white font-extralight"}>POZNAJ NASZE KOŁO NAUKOWE!</p>
-            </div>
-        </>
-    )
-}
