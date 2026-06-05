120fde155a256fc21843f2eeb8ffc5b086892222
MooZeK <mooz.05@hotmail.com>
2026-06-05 21:19:37 +0200

    added Sections.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,15 @@ import './App.css'
 import {Header} from "./header/Header.tsx";
 import Hello from "./hello/Hello.tsx";
 import About from "./about/About.tsx";
+import Sections from "./sections/Sections.tsx";
 
 function App() {
- return(<>
+ return(<div className={"page"}>
      <Header/>
      <Hello/>
      <About/>
-
- </>)
+     <Sections/>
+ </div>)
 }
 
 export default App
