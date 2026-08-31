f225d8411defa63244ef42d1618e1cfd5e307f9c
MooZeK <mooz.05@hotmail.com>
2026-08-31 23:46:15 +0200

    fixed import in scrollhook

--- src/lib/useScrollAndWidth.tsx
+++ src/lib/useScrollAndWidth.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, RefObject } from 'react';
+import { useState, useEffect, type RefObject } from 'react';
 
 export function useScrollAndWidth(
   headerRef: RefObject<HTMLElement | null>,
