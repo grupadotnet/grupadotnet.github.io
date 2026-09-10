43ddfd7e19c6df7da9a249c9c35a30e7add8131f
MooZeKMZK <mooz.05@hotmail.com>
2026-09-10 20:37:11 +0200

    chore: remove react-intersection-observer dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
@@ -8,39 +8,48 @@ import { fileURLToPath } from 'url';
 
 // https://vite.dev/config/
 
-export default defineConfig({
-  plugins: [
-    tanstackRouter(),
-    react(),
-    tailwindcss(),
-    {
-      name: 'copy-index-to-404',
-      writeBundle() {
-        // Copies index.html to 404.html after every build
-        const distPath = path.resolve(
+export default defineConfig(({ mode }) => {
+  // 1. Load env variables
+  const env = loadEnv(mode, process.cwd(), '');
+  const secretHosts = env.TAILSCALE_HOSTS ? env.TAILSCALE_HOSTS.split(',') : [];
+
+  // 2. Return your configuration object
+  return {
+    plugins: [
+      tanstackRouter(),
+      react(),
+      tailwindcss(),
+      {
+        name: 'copy-index-to-404',
+        writeBundle() {
+          // Copies index.html to 404.html after every build
+          const distPath = path.resolve(
+            path.dirname(fileURLToPath(import.meta.url)),
+            'dist'
+          );
+          fs.copyFileSync(
+            path.join(distPath, 'index.html'),
+            path.join(distPath, '404.html')
+          );
+        },
+      },
+    ],
+    server: {
+      // Allows Vite to accept connections over the Tailscale network interface
+      host: true,
+      allowedHosts: [
+        ...secretHosts, // Injects the hidden URLs from .env
+        'grupadotnet.github.io',
+      ],
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(
           path.dirname(fileURLToPath(import.meta.url)),
-          'dist'
-        );
-        fs.copyFileSync(
-          path.join(distPath, 'index.html'),
-          path.join(distPath, '404.html')
-        );
+          './src'
+        ),
       },
     },
-  ],
-  server: {
-    // Allows Vite to accept connections over the Tailscale network interface
-    host: true,
-    allowedHosts: [
-      'moozek-laptop.taile71b45.ts.net',
-      'moozek-pc.taile71b45.ts.net', // Include if not using Tailscale HTTPS
-      'grupadotnet.github.io',
-    ],
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(path.dirname(fileURLToPath(import.meta.url)), './src'),
-    },
-  },
-  ssr: { noExternal: ['maplibre-gl'] },
+    ssr: { noExternal: ['maplibre-gl'] },
+  };
 });
